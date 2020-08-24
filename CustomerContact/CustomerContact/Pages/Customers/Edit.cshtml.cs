e0e607f1d42ec3baa2afedc0988990cefd1d581c
William Gibbons <wgibbons@dacorum.gov.uk>
2020-08-24 14:13:37 +0100

    Implement edit customer

---
+++ CustomerContact/CustomerContact/Pages/Customers/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerContact.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CustomerContact.Pages.Customers
+{
+    public class EditModel : PageModel
+    {
+        private CustomerContext CustomerContext { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public EditModel(CustomerContext customerContext) {
+            CustomerContext = customerContext;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id) {
+            if (id == null) {
+                return BadRequest();
+            }
+
+            Customer = await CustomerContext.Customer.FindAsync(id);
+            if (Customer == null) {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            CustomerContext.Customer.Attach(Customer);
+            await CustomerContext.SaveChangesAsync();
+
+            return RedirectToPage("Details", new { Id = Customer.CustomerID });
+        }
+    }
+}
